3ce59f709ef770ab1f7f15c745960883f9c38bcd
Halil <78084765+halilkaran@users.noreply.github.com>
2022-03-17 22:01:21 +0100

    fix3

--- src/components/addTask/AddTasks.jsx
+++ src/components/addTask/AddTasks.jsx
@@ -1,16 +1,41 @@
 import {React, useState} from "react";
 import "./AddTask.css"
 function AddTasks() {
+
+
+
+
+
 const [task, setTask] = useState([])
     const addbar = (e) => {
         e.preventDefault()
         let a = e.target.parentNode.parentNode.querySelector("#name").value;
         let b = e.target.parentNode.parentNode.querySelector("#Day-time").value;
         setTask([...task, { taske:a , time: b} ])
-        
-        
+        e.target.parentNode.parentNode.querySelector("#name").value=""
+        e.target.parentNode.parentNode.querySelector("#Day-time").value=""
 }
 
+  
+  const deleteBtns =(e)=>  {
+    
+      e.target.parentNode.remove();
+    };
+  
+  const chckBtns = (e) => {
+    
+    if ((e.target.previousElementSibling.className != "liIDchck")) {
+      e.target.previousElementSibling.className = "liIDchck";
+    } else {
+      e.target.previousElementSibling.className = "liID";
+    }
+      
+ }
+
+
+  
+  
+  
 
 
     return <div className="container  text-center mt-4  ">
@@ -30,21 +55,35 @@ const [task, setTask] = useState([])
       
       <div className="submit">
         <button type="submit" onClick={addbar} className="btnn "> <span>Save Task </span> </button>
-        <div className="ease"></div>
+              <div className="ease"></div>
+
+              <div  id="listCon">
+{ task.map((data) => {  
+                    return (
+                      <>
+                        <div id="fadeInID">
+                          <div className="liID">
+                            •&nbsp;&nbsp; {data.taske} {data.time}
+                          </div>
+                          <div onClick={chckBtns} id="checkID">
+                            ✔
+                          </div>
+                          <div onClick={deleteBtns} id="delID">
+                            ✖
+                          </div>
+                        </div>
+                      </>
+                    );  
+                   
+                   
+                })}
+ </div>
       </div>
     </form>
   </div>
 
 </div>
-        <div    className="taskprint">
-            {
-                task.map((data) => {
-                    return <h1>{data.taske} {data.time}</h1>;  
-                   
-                   
-                })
-            }
-</div>
+  
         
     </div>
 }
