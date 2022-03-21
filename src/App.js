36cbff518c3ffc03269759d864248f58721621df
Halil <78084765+halilkaran@users.noreply.github.com>
2022-03-21 08:10:40 +0100

    v2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css"
+import "./components/addTask/AddTask.css"
 import { React, useState } from "react";
  
 import Header from "./components/header/Header";
+import Tasks from "./components/tasks/Tasks";
 import AddTasks from "./components/addTask/AddTasks";
 function App() {
   const [toggle, setToggle] = useState(false);
+  const [todos, setTodos] = useState([]);
 
     const getbtn = () => {
       setToggle(!toggle);
     };
   return (
-    <>
+    <div className="contasks">
       <Header />
       <button onClick={getbtn} className="btnn ">
         {!toggle ? (
@@ -19,9 +22,20 @@ function App() {
           <span>CLOSE ADD TASK BAR</span>
         )}
       </button>
-
-      {toggle && <AddTasks />}
-    </>
+      <div className="container  text-center mt-4  ">
+        <div id="form-main">
+          <div id="form-div">
+            {toggle && <AddTasks setTodos={setTodos} todos={todos} />}
+            <div className="contask">
+             <Tasks todos={todos} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 export default App;
+
+
+
--- src/components/addTask/AddTasks.jsx
+++ src/components/addTask/AddTasks.jsx
@@ -1,91 +1,53 @@
-import {React, useState} from "react";
-import "./AddTask.css"
-function AddTasks() {
+import "./AddTask.css";
+function AddTasks({ todos, setTodos }) {
+  const addbar = (e) => {
 
-
-
-
-
-const [task, setTask] = useState([])
-    const addbar = (e) => {
-        e.preventDefault()
-        let a = e.target.parentNode.parentNode.querySelector("#name").value;
-        let b = e.target.parentNode.parentNode.querySelector("#Day-time").value;
-        setTask([...task, { taske:a , time: b} ])
-        e.target.parentNode.parentNode.querySelector("#name").value=""
-        e.target.parentNode.parentNode.querySelector("#Day-time").value=""
-}
-
-  
-  const deleteBtns =(e)=>  {
     
-      e.target.parentNode.remove();
-    };
-  
-  const chckBtns = (e) => {
-    
-    if ((e.target.previousElementSibling.className !== "liIDchck")) {
-      e.target.previousElementSibling.className = "liIDchck";
-    } else {
-      e.target.previousElementSibling.className = "liID";
+      let a =
+        e.target.parentNode.parentNode.querySelector("#name").value ;
+      let b = e.target.parentNode.parentNode.querySelector("#Day-time").value;
+    if (a && b) {
+      setTodos([...todos, { taske: a, time: b }]);
     }
-      
- }
-
-
   
-  
-  
+   
+     
+  };
 
+  return (
+     
+     
+          <div className="form" id="form1">
+            <p className="Task">
+              <input
+                name="Task"
+                type="text"
+                className="feedback-input"
+                placeholder="Task"
+                id="name"
+              />
+            </p>
 
-    return <div className="container  text-center mt-4  ">
-<div id="form-main">
-  <div id="form-div">
-    <form className="form" id="form1">
-      
-      <p className="Task">
-        <input name="Task" type="text" className="feedback-input" placeholder="Task" id="name" />
-      </p>
-      
-      <p className="time">
-        <input name="Day-time" type="text" className=" feedback-input" id="Day-time" placeholder="Day & Time" />
-      </p>
-      
-      
-      
-      <div className="submit">
-        <button type="submit" onClick={addbar} className="btnn "> <span>Save Task </span> </button>
-              <div className="ease"></div>
-
-              <div  id="listCon">
-{ task.map((data) => {  
-                    return (
-                      <>
-                        <div id="fadeInID">
-                          <div className="liID">
-                            •&nbsp;&nbsp; {data.taske} {data.time}
-                          </div>
-                          <div onClick={chckBtns} id="checkID">
-                            ✔
-                          </div>
-                          <div onClick={deleteBtns} id="delID">
-                            ✖
-                          </div>
-                        </div>
-                      </>
-                    );  
-                   
-                   
-                })}
- </div>
-      </div>
-    </form>
-  </div>
-
-</div>
-  
+            <p className="time">
+        <input
         
-    </div>
+                name="Day-time"
+                type="date"
+                className=" feedback-input"
+                id="Day-time"
+                placeholder="Day & Time"
+              />
+            </p>
+
+            <div className="submit">
+              <button   onClick={addbar} className="btnn ">
+                <span>Save Task </span>
+              </button>
+              <div className="ease"></div>
+            </div>
+          </div>
+      
+  );
 }
 
 export default AddTasks;
